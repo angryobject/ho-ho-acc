5ec1ac70d3e9fbbac9d976525dcf00f582e8a696
Max Shishkin <shishkin.max@gmail.com>
2012-08-05 14:20:13 +0400

    Fixed some jslint errors

--- js/hohoacc.js
+++ js/hohoacc.js
@@ -1,56 +1,55 @@
-'use strict';
-
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
+  "use strict";
 
   var priv = {
-    use: function(fn, obj, args) {
+    use: function (fn, obj, args) {
       args = args || [];
       return priv[fn].apply(obj, args);
     },
 
-    initContentContainers: function() {
+    initContentContainers: function () {
       var self = this,
           container;
 
-      $.each(self.$panels, function() {
+      $.each(self.$panels, function () {
         container = $("<div class=\"content\"></div>");
         container.html(this.html());
         this.html(container);
       });
     },
-    
-    layoutContentContainers: function() {
+
+    layoutContentContainers: function () {
       var self = this,
-    	  	container;
-    		
-      $.each(self.$panels, function() {
+          container;
+
+      $.each(self.$panels, function () {
         container = this.children(".content");
-        
+
         container.css({
           width: self.opts.panelWidth
         });
 
-        if(this.hasClass("panel-centered")) {
-          container.css("left", -self.opts.panelWidth/2);
-        } else if(this.hasClass("panel-righted")) {
+        if (this.hasClass("panel-centered")) {
+          container.css("left", -self.opts.panelWidth / 2);
+        } else if (this.hasClass("panel-righted")) {
           container.css("left", -self.opts.panelWidth);
         }
-      });	
+      });
     },
-    
-    adjustPanelsSize: function() {
+
+    adjustPanelsSize: function () {
       var self = this,
           height = self.$elem.height();
 
-      if(height === 0) {
-        $.each(self.$panels, function() {
-          height = Math.max(this.outerHeight(), height)
+      if (height === 0) {
+        $.each(self.$panels, function () {
+          height = Math.max(this.outerHeight(), height);
         });
 
-        self.$elem.height(height); 
+        self.$elem.height(height);
       }
 
-      $.each(self.$panels, function() {
+      $.each(self.$panels, function () {
         this.css({
           width: self.opts.teaserExpandedWidth,
           height: height
@@ -58,53 +57,53 @@
       });
     },
 
-    addPanelClasses: function() {
+    addPanelClasses: function () {
       var self = this;
 
-      $.each(self.$panels, function(index) {
-        this.addClass("panel" + (index+1));
+      $.each(self.$panels, function (index) {
+        this.addClass("panel" + (index + 1));
       });
     },
 
-    layoutPanels: function() {
+    layoutPanels: function () {
       var self = this;
 
-      $.each(self.$panels, function(index) {
+      $.each(self.$panels, function (index) {
         this.css({
-          left: index*(self.opts.teaserExpandedWidth+self.opts.gapMax),
+          left: index * (self.opts.teaserExpandedWidth + self.opts.gapMax),
           "z-index": self.$panels.length - index
         });
       });
     },
 
-    cacheCoords: function() {
+    cacheCoords: function () {
       var self = this;
 
-      $.each(self.$panels, function(index) {
-        self.opts.coords[index] = parseInt(this.css("left"));
+      $.each(self.$panels, function (index) {
+        self.opts.coords[index] = parseInt(this.css("left"), 10);
       });
     },
 
-    addListeners: function() {
+    addListeners: function () {
       var self = this;
 
-      $.each(self.$panels, function(index) {
-        this.on("mouseenter", function() {
+      $.each(self.$panels, function (index) {
+        this.on("mouseenter", function () {
           self.expandPanel(index);
-        }).on("mouseleave", function() {
+        }).on("mouseleave", function () {
           self.collapsePanel(index);
         });
       });
     },
 
-    indexBounds: function(index) {
+    indexBounds: function (index) {
       var self = this;
       return ((index >= 0) && (index < self.$panels.length));
     }
-  }
+  };
 
   var api = {
-    expandPanel: function(index) {
+    expandPanel: function (index) {
       var self = this,
           expandIndex = index,
           coords = self.opts.coords,
@@ -116,7 +115,7 @@
           left,
           e;
 
-      if(!priv.use("indexBounds", self, [index])) {
+      if (!priv.use("indexBounds", self, [index])) {
         return;
       }
 
@@ -125,17 +124,17 @@
       e.panelIndex = expandIndex;
       self.$elem.trigger(e);
 
-      $.each(self.$panels, function(index) {
-        if(index != expandIndex) {
+      $.each(self.$panels, function (index) {
+        if (index !== expandIndex) {
           width = self.opts.teaserCollapsedWidth;
-          if(moveLeft) {
+          if (moveLeft) {
             this.addClass("panel-left");
             left = coords[index] - deltaCount;
             deltaCount += deltaWidth + gapDelta;
           } else {
             this.addClass("panel-right");
-            left= coords[index] + deltaCount;
-            deltaCount -= (deltaWidth + gapDelta);  
+            left = coords[index] + deltaCount;
+            deltaCount -= (deltaWidth + gapDelta);
           }
         } else {
           this.addClass("active");
@@ -157,12 +156,12 @@
       self.$elem.trigger(e);
     },
 
-    collapsePanel: function(index) {
+    collapsePanel: function (index) {
       var self = this,
           coords = self.opts.coords,
           e;
 
-      if(!priv.use("indexBounds", self, [index])) {
+      if (!priv.use("indexBounds", self, [index])) {
         return;
       }
 
@@ -171,7 +170,7 @@
       e.panelIndex = index;
       self.$elem.trigger(e);
 
-      $.each(self.$panels, function(index) {
+      $.each(self.$panels, function (index) {
           this.removeClass("active panel-left panel-right");
           this.stop().animate({
             width: self.opts.teaserExpandedWidth,
@@ -185,62 +184,62 @@
       self.$elem.trigger(e);
 
     },
-    
-    rebuild: function() {
-    	var self = this;
-    	
-    	self.opts.panelWidth = self.optsDefault.panelWidth || self.$elem.data("panel-width") || self.$elem.width()*0.7;
 
-			if(typeof self.opts.panelWidth === "string" && self.opts.panelWidth.substring(self.opts.panelWidth.length-1) === "%") {
-				self.opts.panelWidth = parseInt(self.opts.panelWidth, 10)/100 * self.$elem.width();
-			}
-	
-			self.opts.panelWidth <= self.$elem.width() || (
-				self.opts.panelWidth = self.$elem.width()*0.7
-			)
-    
+    rebuild: function () {
+      var self = this;
+
+      self.opts.panelWidth = self.optsDefault.panelWidth || self.$elem.data("panel-width") || self.$elem.width() * 0.7;
+
+      if (typeof self.opts.panelWidth === "string" && self.opts.panelWidth.substring(self.opts.panelWidth.length - 1) === "%") {
+        self.opts.panelWidth = parseInt(self.opts.panelWidth, 10) / 100 * self.$elem.width();
+      }
+
+      self.opts.panelWidth <= self.$elem.width() || (
+        self.opts.panelWidth = self.$elem.width() * 0.7
+      );
+
       self.opts.panelWidth = Math.ceil(self.opts.panelWidth);
 
-    	self.opts.teaserCollapsedWidth = Math.ceil((self.$elem.width() - self.opts.panelWidth - self.opts.gapMin*(self.$panels.length-1))/(self.$panels.length-1));
-    	self.opts.teaserExpandedWidth = Math.ceil((self.$elem.width() - self.opts.gapMax*(self.$panels.length-1))/self.$panels.length);
-	    self.opts.coords = [];
-    
-    	priv.use("layoutContentContainers", self);
-	    priv.use("adjustPanelsSize", self);
-	    priv.use("layoutPanels", self);
-	    priv.use("cacheCoords", self);
-    }
-  }
+      self.opts.teaserCollapsedWidth = Math.ceil((self.$elem.width() - self.opts.panelWidth - self.opts.gapMin * (self.$panels.length - 1)) / (self.$panels.length - 1));
+      self.opts.teaserExpandedWidth = Math.ceil((self.$elem.width() - self.opts.gapMax * (self.$panels.length - 1)) / self.$panels.length);
+      self.opts.coords = [];
 
-  var HoHoAcc = function(opts, elem) {
+      priv.use("layoutContentContainers", self);
+      priv.use("adjustPanelsSize", self);
+      priv.use("layoutPanels", self);
+      priv.use("cacheCoords", self);
+    }
+  };
+
+  var HoHoAcc = function (opts, elem) {
     var self = this;
 
-    self.elem     		= elem;
-    self.opts     		= $.extend({}, $.fn.hoHoAcc.options, opts);
-    self.optsDefault	= $.extend({}, $.fn.hoHoAcc.options, opts);
-    self.$elem    		= $(elem);
-    self.$panels  		= [];
+    self.elem         = elem;
+    self.opts         = $.extend({}, $.fn.hoHoAcc.options, opts);
+    self.optsDefault  = $.extend({}, $.fn.hoHoAcc.options, opts);
+    self.$elem        = $(elem);
+    self.$panels      = [];
 
-    self.$elem.children(".panel").each(function() {
+    self.$elem.children(".panel").each(function () {
       self.$panels.push($(this));
     });
 
-    self.opts.panelWidth = self.opts.panelWidth || self.$elem.data("panel-width") || self.$elem.width()*0.7;
+    self.opts.panelWidth = self.opts.panelWidth || self.$elem.data("panel-width") || self.$elem.width() * 0.7;
 
-    if(typeof self.opts.panelWidth === "string" && self.opts.panelWidth.substring(self.opts.panelWidth.length-1) === "%") {
-      self.opts.panelWidth = parseInt(self.opts.panelWidth, 10)/100 * self.$elem.width();
+    if (typeof self.opts.panelWidth === "string" && self.opts.panelWidth.substring(self.opts.panelWidth.length - 1) === "%") {
+      self.opts.panelWidth = parseInt(self.opts.panelWidth, 10) / 100 * self.$elem.width();
     }
 
     self.opts.panelWidth <= self.$elem.width() || (
-      self.opts.panelWidth = self.$elem.width()*0.7
-    )
+      self.opts.panelWidth = self.$elem.width() * 0.7
+    );
 
     self.opts.panelWidth = Math.floor(self.opts.panelWidth);
-    
-    self.opts.teaserCollapsedWidth = Math.floor((self.$elem.width() - self.opts.panelWidth - self.opts.gapMin*(self.$panels.length-1))/(self.$panels.length-1));
-    self.opts.teaserExpandedWidth = Math.floor((self.$elem.width() - self.opts.gapMax*(self.$panels.length-1))/self.$panels.length);
+
+    self.opts.teaserCollapsedWidth = Math.floor((self.$elem.width() - self.opts.panelWidth - self.opts.gapMin * (self.$panels.length - 1)) / (self.$panels.length - 1));
+    self.opts.teaserExpandedWidth = Math.floor((self.$elem.width() - self.opts.gapMax * (self.$panels.length - 1)) / self.$panels.length);
     self.opts.coords = [];
-    
+
     priv.use("initContentContainers", self);
     priv.use("layoutContentContainers", self);
     priv.use("adjustPanelsSize", self);
@@ -248,13 +247,13 @@
     priv.use("layoutPanels", self);
     priv.use("cacheCoords", self);
     priv.use("addListeners", self);
-  }
+  };
 
   HoHoAcc.prototype = api;
 
-  $.fn.hoHoAcc = function(opts) {
-    return this.each(function() {
-      var hoHoAcc = new HoHoAcc(opts, this);  
+  $.fn.hoHoAcc = function (opts) {
+    return this.each(function () {
+      var hoHoAcc = new HoHoAcc(opts, this);
       $.data(this, 'hoHoAcc', hoHoAcc);
     });
   };
@@ -266,4 +265,4 @@
     speed: 200,
     easing: "swing"
   };
-}(jQuery, window, document))
+}(jQuery, window, document));
