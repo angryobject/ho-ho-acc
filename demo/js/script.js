24627b5a52ebde9a33ed977db51aca0ef44f038f
Max Shishkin <shishkin.max@gmail.com>
2012-08-19 14:40:13 +0400

    Updated demo page

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,13 +1,15 @@
 (function($, window, document, undefined) {
-  $("#about-example1").hoHoAcc({gapMin: 10, gapMax: 20});
-  $("#about-example2").hoHoAcc();
-  $("#about-example3").hoHoAcc({speed: 600});
-  $("#about-example4").hoHoAcc({gapMin: 5, gapMax: 15});
+  prettyPrint();
 
-  $("#about-example4").on("expandPanelStart", function(e) {
+  $("#example1").hoHoAcc({gapMin: 10, gapMax: 20});
+  $("#example2").hoHoAcc({panelWidth: 668});
+  $("#example3").hoHoAcc({speed: 600});
+  $("#example4").hoHoAcc({gapMin: 5, gapMax: 15});
+
+  $("#example4").on("expandPanelStart", function(e) {
   	$(this).find("h2").addClass("dull");
   });
-  $("#about-example4").on("collapsePanelStart", function(e) {
-    $(this).find("h2").removeClass("dull");	
+  $("#example4").on("collapsePanelStart", function(e) {
+    $(this).find("h2").removeClass("dull");
   });
 })(jQuery, window, document);
